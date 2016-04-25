2fad2d990c513a206b0def354664732114ab66da
kerdel_e <kerdel_e@epitech.eu>
2016-04-24 21:57:53 -0400

    [*] Squellette Editeur

---
+++ src/editeur/get_xml.c
@@ -0,0 +1,16 @@
+/*
+** get_xml.c for  in /Users/ethankerdelhue/Documents/Shared/Raytracer/src/editeur/
+**
+** Made by Ethan Kerdelhue
+** Login   <kerdel_e@epitech.eu>
+**
+** Started on  Mon Apr 25 03:54:51 2016 Ethan Kerdelhue
+** Last update Mon Apr 25 03:55:30 2016 Ethan Kerdelhue
+*/
+
+#include		"main.h"
+
+void			get_xml(char *path)
+{
+  (void) path;
+}
