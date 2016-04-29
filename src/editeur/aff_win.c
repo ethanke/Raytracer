24142178ec1f9cec961714b2cff7885df5ab64ce
kerdel_e <kerdel_e@epitech.eu>
2016-04-29 13:32:06 -0400

    [-] adaptation of new camera system

--- src/editeur/aff_win.c
+++ src/editeur/aff_win.c
@@ -5,7 +5,7 @@
 ** Login   <kerdel_e@epitech.eu>
 **
 ** Started on  Tue Apr 26 10:06:01 2016 Ethan Kerdelhue
-** Last update Tue Apr 26 10:06:33 2016 Ethan Kerdelhue
+** Last update Fri Apr 29 19:31:33 2016 Ethan Kerdelhue
 */
 
 #include		"main.h"
@@ -22,6 +22,10 @@ int			aff_win_prop(int fd, t_prog *prog)
   my_printf(fd, "\t\t<cam_pos>\n\t\t\t<x>%d</x>\n", prog->cam_pos.x);
   my_printf(fd, "\t\t\t<y>%d</y>\n\t\t\t<z>%d</z>\n\t\t</cam_pos>\n",
 	    prog->cam_pos.y, prog->cam_pos.z);
+  my_printf(fd, "\t\t<look_at>\n\t\t\t<x>%d>/x>\n", prog->look_at.x);
+  my_printf(fd, "\t\t\t<y>%d</y>\n\t\t\t<z>%d</z>\n\t\t</look_at>\n",
+	    prog->look_at.y, prog->look_at.z);
+  my_printf(fd, "\t\t<fov>%d</fov>\n", prog->cam_fov.x);
   my_printf(fd, "\t</view>\n");
   return (0);
 }
