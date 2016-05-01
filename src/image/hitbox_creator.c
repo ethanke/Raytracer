cb7f125d69cd64ed7c1bb1caff0d0eafc7993880
leandr_g <gaetan.leandre@epitech.eu>
2016-05-01 15:48:22 +0200

    [-]modification et gestion erreur de blit dans texture

--- src/image/hitbox_creator.c
+++ src/image/hitbox_creator.c
@@ -5,7 +5,7 @@
 ** Login   <leandr_g@epitech.eu>
 **
 ** Started on  Wed Apr  6 23:47:57 2016 Gaëtan Léandre
-** Last update Sun May  1 15:29:47 2016 Gaëtan Léandre
+** Last update Sun May  1 15:47:42 2016 Gaëtan Léandre
 */
 
 #include 		"main.h"
@@ -14,6 +14,7 @@ void		place_into_hitbox(t_texture *dest, t_texture *src,
 				  t_hitbox hit)
 {
   t_bunny_position	tmp;
+  t_bunny_position	pos;
   int			i;
   int			j;
 
@@ -25,12 +26,12 @@ void		place_into_hitbox(t_texture *dest, t_texture *src,
       tmp.x = hit.x;
       while (j < hit.width)
 	{
-	  if (tmp.x < dest->width && tmp.y < dest->height
-	      && tmp.x >= 0 && tmp.y >= 0)
-	    {
-	    dest->color[tmp.y][tmp.x].full
-	      = src->color[i][j].full;
-	    }
+	  pos.y = (int)(((float)i / (float)hit.height) * (float)(src->height));
+	  pos.x = (int)(((float)j / (float)hit.width) * (float)(src->width));
+	  if (tmp.x < dest->width && tmp.y < dest->height && tmp.x >= 0
+	      && tmp.y >= 0 && pos.x < src->width && pos.y < src->height
+	      && pos.x >= 0 && pos.y >= 0)
+	    dest->color[tmp.y][tmp.x].full = src->color[pos.y][pos.x].full;
 	  tmp.x++;
 	  j++;
 	}
