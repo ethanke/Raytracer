527d3c7a6de3ba36512a3eacf364aef073b6c762
kerdel_e <kerdel_e@epitech.eu>
2016-05-06 00:17:08 +0200

    [*] add loading of .xml (cpp)

--- bonus/mainwindow.cpp
+++ bonus/mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "scene.h"
 #include "addobjectpopup.h"
+#include "QFileDialog.h"
+#include <string.h>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -25,4 +29,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::keyPressEvent(QKeyEvent *e)
+{
+    if (e->key() == Qt::Key_Escape)
+        close();
+    else
+        QWidget::keyPressEvent(e);
+}
 
+void MainWindow::on_loadButton_clicked()
+{
+    Scene *scene = new Scene();
+    scene->path_file = QFileDialog::getOpenFileName(this, tr("Open File"),"/path/to/file/",tr("Files Xml's only (*.xml)"));
+    scene->file = new QFile(scene->path_file);
+}
--- bonus/mainwindow.h
+++ bonus/mainwindow.h
@@ -2,7 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QKeyEvent>
 #include "addobjectpopup.h"
+#include "scene.h"
 
 namespace Ui {
 class MainWindow;
@@ -16,9 +18,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    void keyPressEvent(QKeyEvent *event);
+
+
 public slots:
     int CloseWindow();
     void show_pop();
+
+private slots:
+    void on_loadButton_clicked();
 
 private:
     Ui::MainWindow *ui;
